00822c782d4c145a83ddfa8114d4f103c7414560
scloudyy <onecloud.shen@gmail.com>
2018-09-04 10:54:05 +0800

    feat: add trade and goods

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
+import sys
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+sys.path.insert(0, BASE_DIR)
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))  # then we can directly import model in apps
+sys.path.insert(0, os.path.join(BASE_DIR, '3d_apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
