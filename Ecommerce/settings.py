bdf6e7e033e4316e9370d9c015159598ede0ec1f
scloudyy <onecloud.shen@gmail.com>
2018-09-04 14:38:30 +0800

    fix: add 3d app and trade app

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -39,7 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users'
+    'users',
+    'goods',
+    'trade',
+    'user_operation',
+    'DjangoUeditor'
 ]
 
 MIDDLEWARE = [
--- apps/goods/models.py
+++ apps/goods/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from django.db import models
+from DjangoUeditor.models import UEditorField
 
 
 class GoodsCategory(models.Model):
@@ -25,6 +26,55 @@ class GoodsCategory(models.Model):
     class Meta:
         verbose_name = "goods category"
         verbose_name_plural = "goods categories"
+
+    def __str__(self):
+        return self.name
+
+
+class GoodsCategoryBrand(models.Model):
+    """
+    品牌名
+    """
+    category = models.ForeignKey(GoodsCategory, related_name='brands', null=True, blank=True, verbose_name="category name")
+    name = models.CharField(default="", max_length=30, verbose_name="brand name", help_text="brand name")
+    desc = models.TextField(default="", max_length=200, verbose_name="brand description", help_text="brand description")
+    image = models.ImageField(max_length=200, upload_to="brands/images/")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="added time")
+
+    class Meta:
+        verbose_name = "brand"
+        verbose_name_plural = "brands"
+        db_table = "goods_goodsbrand"
+
+    def __str__(self):
+        return self.name
+
+
+class Goods(models.Model):
+    """
+    goods
+    """
+    category = models.ForeignKey(GoodsCategory, verbose_name="category name")
+    goods_sn = models.CharField(max_length=50, default="", verbose_name="good sn")
+    name = models.CharField(max_length=100, verbose_name="good name")
+    click_num = models.IntegerField(default=0, verbose_name="click name")
+    sold_num = models.IntegerField(default=0, verbose_name="sold number")
+    fav_num = models.IntegerField(default=0, verbose_name="favorite number")
+    goods_num = models.IntegerField(default=0, verbose_name="goods number")
+    market_price = models.FloatField(default=0, verbose_name="market price")
+    shop_price = models.FloatField(default=0, verbose_name="shop price")
+    goods_brief = models.TextField(max_length=500, verbose_name="goods brief description")
+    goods_desc = UEditorField(verbose_name="content", imagePath="goods/images/", width=1000, height=300,
+                              filePath="goods/files/", default='')
+    ship_free = models.BooleanField(default=True, verbose_name="whether ship is free")
+    goods_front_image = models.ImageField(upload_to="goods/images/", null=True, blank=True, verbose_name="goods images")
+    is_new = models.BooleanField(default=False, verbose_name="whether is new goods")
+    is_hot = models.BooleanField(default=False, verbose_name="whether is hot goods")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="added time")
+
+    class Meta:
+        verbose_name = 'good'
+        verbose_name_plural = 'goods'
 
     def __str__(self):
         return self.name
