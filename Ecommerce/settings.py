2b58ebe4e942b299993a3853063c1bc448b581c3
scloudyy <onecloud.shen@gmail.com>
2018-09-04 10:21:40 +0800

    feat: change db to mysql

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -76,7 +76,11 @@ WSGI_APPLICATION = 'Ecommerce.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Ecommerce',
+        'NAME': "Ecommerce",
+        'USER': 'root',
+        'PASSWORD': "root",
+        'HOST': "127.0.0.1",
+        'OPTIONS': {'init_command': 'SET storage_engine=INNODB;'}
     }
 }
 
