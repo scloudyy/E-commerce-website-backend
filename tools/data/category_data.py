ba161b661c5c79e6792dcb0a7a99ec1aded14a8a
scloudyy <onecloud.shen@gmail.com>
2018-09-04 16:29:13 +0800

    chore: add test data

---
+++ tools/data/category_data.py
@@ -0,0 +1,227 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+row_data = [
+    {
+        'sub_categorys': [
+            {
+                'sub_categorys': [
+                    {
+                        'code': 'apple',
+                        'name': 'Apple'
+                    },
+                    {
+                        'code': 'thinkpad',
+                        'name': 'ThinkPad'
+                    },
+                ],
+                'code': 'computer',
+                'name': 'Computer Category'
+            },
+            {
+                'sub_categorys': [
+                    {
+                        'code': 'apple',
+                        'name': 'Apple'
+                    },
+                    {
+                        'code': 'xiaomi',
+                        'name': 'xiaomi'
+                    },
+                    {
+                        'code': 'huawei',
+                        'name': 'Huawei'
+                    },
+                ],
+                'code': 'phone',
+                'name': 'Phone'
+            },
+            {
+                'sub_categorys': [
+                    {
+                        'code': 'apple',
+                        'name': 'Apple'
+                    },
+                    {
+                        'code': 'xiaomi',
+                        'name': 'xiaomi'
+                    },
+                    {
+                        'code': 'huawei',
+                        'name': 'Huawei'
+                    }
+                ],
+                'code': 'watch',
+                'name': 'Watch'
+            },
+            {
+                'sub_categorys': [
+                    {
+                        'code': 'apple',
+                        'name': 'Apple'
+                    },
+                    {
+                        'code': 'xiaomi',
+                        'name': 'Xiaomi'
+                    },
+                    {
+                        'code': 'tcl',
+                        'name': 'TCL'
+                    },
+                ],
+                'code': 'tv',
+                'name': 'Television'
+            },
+        ],
+        'code': 'electronics',
+        'name': 'Electronics'
+    },
+    {
+        'sub_categorys': [
+            {
+                'sub_categorys': [
+                    {
+                        'code': 'apple',
+                        'name': 'Apple'
+                    },
+                    {
+                        'code': 'thinkpad',
+                        'name': 'ThinkPad'
+                    },
+                ],
+                'code': 'computer2',
+                'name': 'Computer2'
+            },
+            {
+                'sub_categorys': [
+                    {
+                        'code': 'apple',
+                        'name': 'Apple'
+                    },
+                    {
+                        'code': 'xiaomi',
+                        'name': 'xiaomi'
+                    },
+                    {
+                        'code': 'huawei',
+                        'name': 'Huawei'
+                    },
+                ],
+                'code': 'phone2',
+                'name': 'Phone2'
+            },
+            {
+                'sub_categorys': [
+                    {
+                        'code': 'apple',
+                        'name': 'Apple'
+                    },
+                    {
+                        'code': 'xiaomi',
+                        'name': 'xiaomi'
+                    },
+                    {
+                        'code': 'huawei',
+                        'name': 'Huawei'
+                    }
+                ],
+                'code': 'watch2',
+                'name': 'Watch2'
+            },
+            {
+                'sub_categorys': [
+                    {
+                        'code': 'apple',
+                        'name': 'Apple'
+                    },
+                    {
+                        'code': 'xiaomi',
+                        'name': 'Xiaomi'
+                    },
+                    {
+                        'code': 'tcl',
+                        'name': 'TCL'
+                    },
+                ],
+                'code': 'tv2',
+                'name': 'Television2'
+            },
+        ],
+        'code': 'electronics2',
+        'name': 'Electronics2'
+    },
+    {
+        'sub_categorys': [
+            {
+                'sub_categorys': [
+                    {
+                        'code': 'apple',
+                        'name': 'Apple'
+                    },
+                    {
+                        'code': 'thinkpad',
+                        'name': 'ThinkPad'
+                    },
+                ],
+                'code': 'computer3',
+                'name': 'Computer3'
+            },
+            {
+                'sub_categorys': [
+                    {
+                        'code': 'apple',
+                        'name': 'Apple'
+                    },
+                    {
+                        'code': 'xiaomi',
+                        'name': 'xiaomi'
+                    },
+                    {
+                        'code': 'huawei',
+                        'name': 'Huawei'
+                    },
+                ],
+                'code': 'phone3',
+                'name': 'Phone3'
+            },
+            {
+                'sub_categorys': [
+                    {
+                        'code': 'apple',
+                        'name': 'Apple'
+                    },
+                    {
+                        'code': 'xiaomi',
+                        'name': 'xiaomi'
+                    },
+                    {
+                        'code': 'huawei',
+                        'name': 'Huawei'
+                    }
+                ],
+                'code': 'watch3',
+                'name': 'Watch3'
+            },
+            {
+                'sub_categorys': [
+                    {
+                        'code': 'apple',
+                        'name': 'Apple'
+                    },
+                    {
+                        'code': 'xiaomi',
+                        'name': 'Xiaomi'
+                    },
+                    {
+                        'code': 'tcl',
+                        'name': 'TCL'
+                    },
+                ],
+                'code': 'tv3',
+                'name': 'Television3'
+            },
+        ],
+        'code': 'electronics3',
+        'name': 'Electronics3'
+    },
+]
---
+++ tools/data/product_data.py
@@ -0,0 +1,225 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+row_data = [
+    {
+        'images': [
+            'goods/images/computer1.jpg',
+            'goods/images/computer2.jpg',
+            'goods/images/computer3.jpg',
+            'goods/images/computer4.jpg',
+        ],
+        'categorys': [
+            'index',
+            'Electronics',
+            'Computer'
+        ],
+        'market_price': '128',
+        'name': 'computer1',
+        'desc': 'test',
+        'sale_price': '256',
+        'goods_desc':'test'
+    },
+    {
+        'images': [
+            'goods/images/phone1.jpg',
+            'goods/images/phone2.jpg',
+            'goods/images/phone3.jpg',
+            'goods/images/phone4.jpg',
+        ],
+        'categorys': [
+            'index',
+            'Electronics',
+            'Phone'
+        ],
+        'market_price': '128',
+        'name': 'phone1',
+        'desc': 'test',
+        'sale_price': '256',
+        'goods_desc':'test'
+    },
+    {
+        'images': [
+            'goods/images/watch1.jpg',
+            'goods/images/watch2.jpg',
+            'goods/images/watch3.jpg',
+            'goods/images/watch4.jpg',
+        ],
+        'categorys': [
+            'index',
+            'Electronics',
+            'Watch'
+        ],
+        'market_price': '128',
+        'name': 'watch1',
+        'desc': 'test',
+        'sale_price': '256',
+        'goods_desc':'test'
+    },
+    {
+        'images': [
+            'goods/images/tv1.jpg',
+            'goods/images/tv2.jpg',
+            'goods/images/tv3.jpg',
+            'goods/images/tv4.jpg',
+        ],
+        'categorys': [
+            'index',
+            'Electronics',
+            'Television'
+        ],
+        'market_price': '128',
+        'name': 'tv1',
+        'desc': 'test',
+        'sale_price': '256',
+        'goods_desc':'test'
+    },
+    #####################################
+    {
+        'images': [
+            'goods/images/computer1.jpg',
+            'goods/images/computer2.jpg',
+            'goods/images/computer3.jpg',
+            'goods/images/computer4.jpg',
+        ],
+        'categorys': [
+            'index',
+            'Electronics2',
+            'Computer2'
+        ],
+        'market_price': '128',
+        'name': 'computer12',
+        'desc': 'test2',
+        'sale_price': '256',
+        'goods_desc':'test2'
+    },
+    {
+        'images': [
+            'goods/images/phone1.jpg',
+            'goods/images/phone2.jpg',
+            'goods/images/phone3.jpg',
+            'goods/images/phone4.jpg',
+        ],
+        'categorys': [
+            'index',
+            'Electronics2',
+            'Phone2'
+        ],
+        'market_price': '128',
+        'name': 'phone12',
+        'desc': 'test2',
+        'sale_price': '256',
+        'goods_desc':'test2'
+    },
+    {
+        'images': [
+            'goods/images/watch1.jpg',
+            'goods/images/watch2.jpg',
+            'goods/images/watch3.jpg',
+            'goods/images/watch4.jpg',
+        ],
+        'categorys': [
+            'index',
+            'Electronics2',
+            'Watch2'
+        ],
+        'market_price': '128',
+        'name': 'watch12',
+        'desc': 'test2',
+        'sale_price': '256',
+        'goods_desc':'test2'
+    },
+    {
+        'images': [
+            'goods/images/tv1.jpg',
+            'goods/images/tv2.jpg',
+            'goods/images/tv3.jpg',
+            'goods/images/tv4.jpg',
+        ],
+        'categorys': [
+            'index',
+            'Electronics2',
+            'Television2'
+        ],
+        'market_price': '128',
+        'name': 'tv12',
+        'desc': 'test2',
+        'sale_price': '256',
+        'goods_desc':'test2'
+    },
+    ######################################
+    {
+        'images': [
+            'goods/images/computer1.jpg',
+            'goods/images/computer2.jpg',
+            'goods/images/computer3.jpg',
+            'goods/images/computer4.jpg',
+        ],
+        'categorys': [
+            'index',
+            'Electronics3',
+            'Computer3'
+        ],
+        'market_price': '128',
+        'name': 'computer13',
+        'desc': 'test3',
+        'sale_price': '256',
+        'goods_desc':'test3'
+    },
+    {
+        'images': [
+            'goods/images/phone1.jpg',
+            'goods/images/phone2.jpg',
+            'goods/images/phone3.jpg',
+            'goods/images/phone4.jpg',
+        ],
+        'categorys': [
+            'index',
+            'Electronics3',
+            'Phone3'
+        ],
+        'market_price': '128',
+        'name': 'phone13',
+        'desc': 'test3',
+        'sale_price': '256',
+        'goods_desc':'test3'
+    },
+    {
+        'images': [
+            'goods/images/watch1.jpg',
+            'goods/images/watch2.jpg',
+            'goods/images/watch3.jpg',
+            'goods/images/watch4.jpg',
+        ],
+        'categorys': [
+            'index',
+            'Electronics3',
+            'Watch3'
+        ],
+        'market_price': '128',
+        'name': 'watch13',
+        'desc': 'test3',
+        'sale_price': '256',
+        'goods_desc':'test3'
+    },
+    {
+        'images': [
+            'goods/images/tv1.jpg',
+            'goods/images/tv2.jpg',
+            'goods/images/tv3.jpg',
+            'goods/images/tv4.jpg',
+        ],
+        'categorys': [
+            'index',
+            'Electronics3',
+            'Television3'
+        ],
+        'market_price': '128',
+        'name': 'tv13',
+        'desc': 'test3',
+        'sale_price': '256',
+        'goods_desc':'test3'
+    },
+]
+
+pass
